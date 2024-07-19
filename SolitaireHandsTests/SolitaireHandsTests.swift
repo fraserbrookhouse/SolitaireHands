a1cfb7c8aee01bfe2ca91a953fb9a4fcfff24621
Fraser Brookhouse <fbrookhouse@gmail.com>
2024-07-19 22:15:00 +0100

    Initial Commit

---
+++ SolitaireHandsTests/SolitaireHandsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SolitaireHandsTests.swift
+//  SolitaireHandsTests
+//
+//  Created by Fraser Brookhouse on 19/07/2024.
+//
+
+import Testing
+@testable import SolitaireHands
+
+struct SolitaireHandsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
